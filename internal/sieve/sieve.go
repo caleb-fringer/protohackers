de40ef6e2439bea40050a91ba3403b14dc653501
Caleb Fringer <cfringer98@gmail.com>
2026-08-12 14:55:27 -0700

    Refactor Sieve to use byte manip., encapsulate IsPrime logic.

--- internal/sieve/sieve.go
+++ internal/sieve/sieve.go
@@ -1,24 +1,59 @@
 package sieve
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func NewSieve(n int) []bool {
-	sieve := make([]bool, n)
+type Sieve struct {
+	n    int
+	nums []byte
+}
 
-	for i := 2; i < n; i++ {
-		sieve[i] = true
+func (s Sieve) markComposite(i int) error {
+	if i < 0 || i >= s.n {
+		return fmt.Errorf("Index %v is out of bounds", i)
 	}
+	byteIndex := i / 8
+	byteOffset := i % 8
+	s.nums[byteIndex] &= ^(1 << byteOffset)
+	return nil
+}
+
+func (s Sieve) IsPrime(i int) bool {
+	if i < 0 || i >= s.n {
+		return false
+	}
+
+	byteIndex := i / 8
+	byteOffset := i % 8
+	return s.nums[byteIndex]&(1<<byteOffset) > 0
+}
+
+func NewSieve(n int) Sieve {
+	numBytes := int(math.Ceil(float64(n) / 8))
+	sieve := Sieve{
+		n:    n,
+		nums: make([]byte, numBytes),
+	}
+
+	// Initialize the array to true
+	for i := range numBytes {
+		sieve.nums[i] = 0xFF
+	}
+	// Mark 0 and 1 as composite.
+	sieve.nums[0] &= 0xFC
 
 	root_n := int(math.Sqrt(float64(n)))
 	for i := 2; i <= root_n; {
 		for j := i; i*j < n; j++ {
-			sieve[i*j] = false
+			sieve.markComposite(i * j)
 		}
-		nextPrime := i + 1
-		for nextPrime < n && sieve[nextPrime] == false {
-			nextPrime++
+		primeCandidate := i + 1
+		for primeCandidate < n && !sieve.IsPrime(primeCandidate) {
+			primeCandidate++
 		}
-		i = nextPrime
+		i = primeCandidate
 	}
 	return sieve
 }
--- internal/sieve/sieve_test.go
+++ internal/sieve/sieve_test.go
@@ -1,17 +1,32 @@
 package sieve
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestSieve(t *testing.T) {
 	test_cases := []bool{false, false, true, true, false, true, false, true, false, false, false, true, false}
 
 	sieve := NewSieve(len(test_cases))
-	for i, v := range test_cases {
-		isPrime := sieve[i]
-		if isPrime != v {
-			t.Errorf("Invalid prime: Expected %v to be %v, got %v\n", i, v, sieve[i])
+	for num, expected := range test_cases {
+		isPrime := sieve.IsPrime(num)
+		if isPrime != expected {
+			t.Errorf("Invalid prime: Expected %v to be %v, got %v\n", num, expected, isPrime)
 		}
+	}
+}
+
+func TestMarkComposite(t *testing.T) {
+	sieve := Sieve{
+		n:    16,
+		nums: make([]byte, 16/8),
+	}
+	sieve.nums[0] = 0xFF
+	sieve.nums[1] = 0xFF
+
+	err := sieve.markComposite(9)
+	if err != nil {
+		t.Fatalf("Error marking 9 as composite: %v\n", err)
+	}
+	if sieve.IsPrime(9) {
+		t.Errorf("Sieve.markComposite(9) failed to mark 9 as composite\n")
 	}
 }
