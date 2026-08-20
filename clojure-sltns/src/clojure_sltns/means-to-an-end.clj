56e03fe472ddfd795dda60a01f994a8cc3c0d269
Caleb Fringer <cfringer98@gmail.com>
2026-08-19 23:27:04 -0700

    Implement basic byte reading loop. Begin writing API for parsing messages.

--- clojure-sltns/src/clojure_sltns/means-to-an-end.clj
+++ clojure-sltns/src/clojure_sltns/means-to-an-end.clj
@@ -1,9 +1,34 @@
 (ns clojure-sltns.means-to-an-end
+  (:import [java.io InputStream OutputStream ByteArrayInputStream IOException]
+           [java.nio.charset StandardCharsets])
   (:require
    [clojure.java.io :as io]))
 
-(defn connection-handler
-  [client-socket]
-  (let [in (io/reader client-socket)]
-    (.read in)))
+(defn parse-msg
+  [^bytes msg])
 
+(defn read-msg
+  "Reads a single, 9 byte msg from in. If the resulting message read is less than 9 bytes, nil is returned."
+  [^InputStream in
+   ^bytes buf]
+  (let [cb (.readNBytes in buf 0 9)]
+    (if (= cb 9) buf nil)))
+
+(defn connection-handler
+  [^InputStream in
+   ^OutputStream out]
+  (let [buf (byte-array 9)]
+    (try
+      (loop [msg (read-msg in buf)]
+        (print msg "\n")
+        (if (not (nil? msg))
+          (recur (read-msg in buf))
+          nil))
+      (catch IOException e
+        (print "Caught exception: " (.getMessage e)))
+      (finally (.close in)))))
+
+(connection-handler
+ (ByteArrayInputStream.
+  (.getBytes "Hello, my name is George clooooooney" StandardCharsets/UTF_8))
+ nil)
